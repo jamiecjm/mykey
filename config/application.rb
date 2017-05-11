0a40d638c4e9b9f3385f25cd53418c2997909420
jamiecjm <jamiecjm@gmail.com>
2017-05-11 18:46:09 +0800

    added jquery-ui css

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Mykey
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.sass.load_paths << File.expand_path('../../lib/assets/stylesheets/')
+    config.sass.load_paths << File.expand_path('../../vendor/assets/stylesheets/')
   end
 end
+
