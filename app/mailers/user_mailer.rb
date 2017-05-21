7f609e7ae556be7283724d36145ff74188dc5b76
jamiecjm <jamiecjm@gmail.com>
2017-05-21 17:45:47 +0800

    add admin email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 class UserMailer < ApplicationMailer
   def change_password(user)
     @user = user
+    email = @user.email
+    email = ENV["email_address"] if @user.email == "admin"
     mail(
       from: Clearance.configuration.mailer_sender,
-      to: @user.email,
+      to: email,
       subject: I18n.t(
         :change_password,
         scope: [:clearance, :models, :clearance_mailer]
